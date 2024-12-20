98b301ad8d87de9e1ca96250b515a94c589becd1
Nguyen Quang Tuan <167944168+tuannguyenquang22@users.noreply.github.com>
2024-12-20 13:50:51 +0700

    update

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -6,7 +6,7 @@ import { logout, refreshTokenAsync } from '../features/auth/authSlice';
 
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
+    baseURL: "http://212.28.182.228:8000/api/v1",
     headers: {
         'Content-Type': 'application/json',
     },
